57afadc16b3d482cbb359542263fc59a65d2bed2
Péter Jeszenszky <jeszenszky.peter@inf.unideb.hu>
2024-04-30 22:18:28 +0200

    refactor: adjust Main to refactored BreadthFirstSearch

--- src/main/java/slidingpuzzle/solver/Main.java
+++ src/main/java/slidingpuzzle/solver/Main.java
@@ -7,7 +7,7 @@ import slidingpuzzle.model.PuzzleState;
 public class Main {
 
     public static void main(String[] args) {
-        var bfs = new BreadthFirstSearch<PuzzleState, Direction>();
+        var bfs = new BreadthFirstSearch<Direction>();
         bfs.solveAndPrintSolution(new PuzzleState());
     }
 
